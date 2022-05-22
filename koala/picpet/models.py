57766f8d4ac9d2cea6500e8274952ec28b5d6ad1
alexisunam <alexisduenasunam@gmail.com>
2022-05-22 16:32:41 -0500

    base de datos actualizada, documentos, artista

--- koala/picpet/models.py
+++ koala/picpet/models.py
@@ -13,11 +13,17 @@ class Persona(models.Model):
     def __str__(self):
         return f'Persona {self.id} : {self.nombre} {self.apellidoPaterno} {self.apellidoMaterno} {self.nombreUsuario} {self.email} {self.edad} {self.contrasenia}'
 
-""" class Documento(models.Model):
+class Documento(models.Model):
     titulo = models.CharField(max_length=255)
     subirArchivo = models.FileField(upload_to="Uploaded Files/")
-    dateTimeOfUpload = models.DataTimeField(auto_now=True)
+    dateTimeOfUpload = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Documento {self.id} : {self.titulo} {self.subirArchivo} {self.dataTimeOfUpload}'
 
 class Artista(Persona):
     numeroCuenta = models.CharField(max_length=9)
-    archivo = Documento()    """
+    archivo = models.ForeignKey(Documento, on_delete=models.RESTRICT, null=False)
+    
+    def __str__(self):
+        return f'Artista {self.id} : {self.numeroCuenta} {self.archivo}' 
